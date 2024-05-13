991b2b3a60e6cf64b4232275e9ea40754bc05eab
wenkai.wang <wenkai.wang@move-x.com.cn>
2024-05-13 15:15:29 +0800

    11

---
+++ build/CMakeFiles/box.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../box"
+  "../box.pdb"
+  "CMakeFiles/box.dir/main.cc.o"
+  "CMakeFiles/box.dir/src/box.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/box.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
