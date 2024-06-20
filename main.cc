b9373e4a8ea33bbcde25c3b787812483f0f71723
wenkai.wang <wenkai.wang@move-x.com.cn>
2024-06-20 18:01:01 +0800

    xxx

--- main.cc
+++ main.cc
@@ -1,114 +1,120 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-typedef int ElemType;
-typedef struct LNode {
-    ElemType data;
-    struct LNode* next;
-} LNode, *LinkList;
- 
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <vector>
+#include <queue>
 
-LinkList init_list()
-{
-    LNode* p = (LNode *)malloc(sizeof(LNode));
-    if(p == NULL)
-    {
-        return NULL;
+using namespace std;
+
+struct TreeNode {
+    int val;//结点值
+    TreeNode *left;//左结点
+    TreeNode *right;//右结点
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root) return ans;
+        dfs(root);
+        return ans;
     }
-    p->next = NULL;
-    return p;
-}
-
-int get_len_lsit(LinkList p)
-{
-    int num = 0;
-    LinkList temp = p;
-    while(NULL != temp)
+    void dfs(TreeNode* root) 
     {
-        temp = temp->next;
-        num++;
-    }
-    return num;
-}
-
-void insert_head(LinkList *p, int data)
-{
-    LNode *cur = (LNode*)malloc(sizeof(LNode));;
-    cur->data = data;
-    cur->next = (*p)->next;
-    (*p)->next = cur;
-}
-
-void insert_tail(LinkList *p, int data)
-{
-    LNode *newNode = (LNode*)malloc(sizeof(LNode));
-    newNode->data = data;
-    newNode->next = NULL; // 新节点的next指针初始化为NULL，因为它将是链表的新尾节点
-    LNode *cur = (*p);
-    while (cur->next)
-    {
-        cur = cur->next;
-    }
-    // 如果链表为空，新节点同时作为头节点和尾节点
-    if (*p == NULL) {
-        *p = newNode;
-    } else { // 否则，找到当前尾节点并将其next指针指向新节点
-        cur->next = newNode;
+        if (!root) return;
+        ans.push_back(root->val);//根
+        dfs(root->left);//左
+        dfs(root->right);//右
     }
 
-}
+/*
+          1
+      2       3
+    4    5  6    7
 
-void insert_mid(LinkList *p, int data, int pos)
-{   
-    
-    LNode *cur = (LNode *)malloc(sizeof(LNode));
-    cur->data = data;
-    LNode *temp = (*p); 
-    for(int i = 0; i < pos; i++)
+*/
+
+    void dfs_mid(TreeNode* root)
     {
-        temp = temp->next;
+        if (!root) return;
+        dfs_mid(root->left);//左
+        ans.push_back(root->val);//根
+        dfs_mid(root->right);//右
     }
-    cur->next = temp->next;
-    temp->next = cur;
-}
+
+    void dfs_ago(TreeNode* root) 
+    {
+        if (!root) return;
+        dfs_ago(root->left);//左
+        dfs_ago(root->right);//右
+        ans.push_back(root->val);//根
+    }
 
 
-void printf_list(LinkList p)
+     vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ansm;
+        if (!root) return ansm;
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty())
+        {
+            vector<int> path;//记录每一层的结点的值
+            int n = que.size();//每一层的结点数
+            while (n--)
+            {
+                root = que.front();
+                path.push_back(root->val);
+                que.pop();
+                if (root->left) que.push(root->left);
+                if (root->right) que.push(root->right);
+            }
+            ansm.push_back(path);
+        }
+        return ansm;
+    }
+
+
+};
+
+
+
+
+/*
+          1
+      2       3
+    4    5  6    7
+
+*/
+int main()
 {
-    LNode *temp = p; 
-    temp = temp->next; 
-    while (NULL != temp)
+    TreeNode *root = new TreeNode(1, nullptr,nullptr);
+    TreeNode *l = new TreeNode(2);
+    TreeNode *r = new TreeNode(3);
+    root->left = l;
+    root->right = r;
+    l->left = new TreeNode(4);
+    l->right = new TreeNode(5);
+    r->left = new TreeNode(6);
+    r->right = new TreeNode(7);
+
+    Solution sl;
+    //sl.dfs(root);
+    auto ptr = sl.levelOrder(root);
+    // sl.dfs(root);
+    // sl.dfs(root);
+
+    for(int i = 0; i < ptr.size(); ++i)
     {
-        printf(" %d", temp->data);
-        temp = temp->next; 
-    }  
-     printf("\n");
-}
-
-int main() {
-   
-    LinkList p = init_list();
-   
-    for(int i = 0; i<20; i++)
-    {
-         /*头插法*/
-       //insert_head(&p,i); 
-
-      // insert_mid(&p,i,i);
-
-       /*尾插法*/
-        insert_tail(&p,i);
-    }  
-     
-    printf_list(p); 
-    printf("链表长度：%d\n", get_len_lsit(p));
-
-    /*中间*/
-    insert_mid(&p, 100, 10);
-
-    printf_list(p); 
-    printf("链表长度：%d\n", get_len_lsit(p));
-
-
+        cout << "+++++++++++ : " << sl.ans[i]<<endl;
+        for(int j = 0; j < ptr[i].size(); ++j)
+        {
+            cout << "+++++++++++ : " << ptr[i][j]<<endl;
+        }
+    }
     return 0;
 }
